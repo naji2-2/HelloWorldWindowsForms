8322847af3bfbf86d0e2f29e00836a7bc425b783
Najisu <jisusunny0225@gmail.com>
2025-04-11 10:22:09 +0900

    About 폼 추가

---
+++ FormAbout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWorldWindowsForms
+{
+	public partial class FormAbout : HelloWorldWindowsForms.FormMain
+	{
+		public FormAbout()
+		{
+			InitializeComponent();
+		}
+	}
+}
